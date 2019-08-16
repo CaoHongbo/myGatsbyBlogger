4a5f93df6c9070c85b5c8c67b60c2b4deab5cb6f
caohongbo <caohongbo@didiglobal.com>
2019-08-16 17:23:10 +0800

    先写一个博客试试水

--- src/website-config.ts
+++ src/website-config.ts
@@ -53,7 +53,7 @@ const config: WebsiteConfig = {
   description: 'The professional publishing platform',
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
-  lang: 'en',
+  lang: 'zh-CN',
   siteUrl: 'https://gatsby-casper.netlify.com',
   facebook: 'https://www.facebook.com/ghost',
   twitter: 'https://twitter.com/tryghost',
